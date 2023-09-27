d16fe6d925688ff26a75ae8589c43be07f900aaf
Bilal Laskri <lsb2874a@u1-105-04d.univ-tlse3.fr>
2023-09-27 15:51:04 +0200

    add high_level models & admin

--- brewsim/high_level/models.py
+++ brewsim/high_level/models.py
@@ -1,22 +1,14 @@
 from django.db import models
 
 # Create your models here.
-models.IntegerField()
 
-class Usine(models.Model):
-	departement = models.ForeignKey(Departement, # ou "self",
-									on_delete=models.PROTECT,
-									# blank=True, null=True,
-									# related_name="+",
-									)
-	taille = models.IntegerField()
-	machines = models.ManyToManyField(Machine)
-	recettes = models.ManyToManyField(Recette)
-	stocks = models.ManyToManyField(QuantiteIngredient)
-		
+
 class Departement(models.Model):
 	numero = models.IntegerField()
 	prixm2 = models.IntegerField()
+
+class Ingredient(models.Model):
+	nom = models.CharField(max_length=100)
 	
 class Prix(models.Model):
 	ingredient = models.ForeignKey(Ingredient, # ou "self",
@@ -35,17 +27,6 @@ class Machine(models.Model):
 	nom = models.CharField(max_length=100)
 	prix = models.IntegerField()
 
-class Recette(models.Model):
-	nom = models.CharField(max_length=100)
-	action = models.ForeignKey(Action, # ou "self",
-									on_delete=models.PROTECT,
-									# blank=True, null=True,
-									# related_name="+",
-									)
-																									
-class Ingredient(models.Model):
-	nom = models.CharField(max_length=100)
-	
 class QuantiteIngredient(models.Model):
 	ingredient = models.ForeignKey(Ingredient, # ou "self",
 									on_delete=models.PROTECT,
@@ -63,8 +44,27 @@ class Action(models.Model):
 	commande = models.CharField(max_length=100)
 	duree = models.IntegerField()
 	ingredients = models.ManyToManyField(QuantiteIngredient)
-	action = models.ForeignKey(self, # ou "self",
+	action = models.ForeignKey("self", # ou "self",
 									on_delete=models.PROTECT,
-									blank=True, null=True
+									blank=True, null=True,
+									related_name="+"
+									)
+
+class Recette(models.Model):
+	nom = models.CharField(max_length=100)
+	action = models.ForeignKey(Action, # ou "self",
+									on_delete=models.PROTECT,
+									# blank=True, null=True,
 									# related_name="+",
 									)
+									
+class Usine(models.Model):
+	departement = models.ForeignKey(Departement, # ou "self",
+									on_delete=models.PROTECT,
+									# blank=True, null=True,
+									# related_name="+",
+									)
+	taille = models.IntegerField()
+	machines = models.ManyToManyField(Machine)
+	recettes = models.ManyToManyField(Recette)
+	stocks = models.ManyToManyField(QuantiteIngredient)
