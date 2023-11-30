26193364514e70a8ece4650d6ecb246ee25ace09
Bilal Laskri <lsb2874a@u1-105-04d.univ-tlse3.fr>
2023-11-30 17:24:44 +0100

    Save

--- brewsim/low_level/low_level.cpp
+++ brewsim/low_level/low_level.cpp
@@ -5,7 +5,6 @@
 
 using json = nlohmann::json;
 
-
 class Departement
 {
 	public :
@@ -13,7 +12,7 @@ class Departement
 		Departement(int numero, int prixm2);
 		
 		// Constructeur JSON
-		Departement(json fich);
+		//Departement(json fich);
 		
 		// Constructeur ID
 		Departement(int id);
@@ -28,15 +27,16 @@ class Departement
 Departement::Departement(int numero, int prixm2) : numero_(numero), prixm2_(prixm2)
 {}
 // Constructeur JSON
-Departement::Departement(json fich) : numero_(fich["numero"]), prixm2_(fich["prixm2"])
-{}
+//Departement::Departement(json fich) : numero_(fich["numero"]), prixm2_(fich["prixm2"])
+//{}
 // Constructeur ID
 Departement::Departement(int id)
 {
 	std::string lien = "http://localhost:8000/departement/" + std::to_string(id);
 	cpr::Response r  = cpr::Get(cpr::Url(lien));
 	json fich = json::parse(r.text);
-	numero_ = fich["numero"]; prixm2_ = fich["prixm2"];
+	numero_ = fich["numero"]; 
+	prixm2_ = fich["prixm2"];
 }
 
 class Ingredient
@@ -45,6 +45,8 @@ class Ingredient
 		// Constructeur ID
 		Ingredient(int id);
 		std::string nom_;
+		
+		void afficher();
 };
 
 class Prix
@@ -56,6 +58,8 @@ class Prix
 		std::unique_ptr<Departement> departement_;
 		int prix_;
 		
+		void afficher();
+		
 };
 
 class Machine
@@ -65,6 +69,8 @@ class Machine
 	
 		std::string nom_;
 		int prix_;
+		
+		void afficher();
 		
 };
 
@@ -77,8 +83,8 @@ class QuantiteIngredient
 		//std::make_unique<Ingredient>(4)
 		int quantite_;
 		
+		void afficher();
 };
-
 
 class Action
 {
@@ -88,20 +94,22 @@ class Action
 		std::unique_ptr<Machine> machine_;
 		std::string commande_;
 		int duree_;
-		std::unique_ptr<QuantiteIngredient> ingredients_;
-		std::optional<std::unique_ptr<Action>> action_;
+		std::vector<std::unique_ptr<QuantiteIngredient>> ingredients_;
+		std::optional<std::unique_ptr<Action>> action_;	
 		
+		void afficher();
 };
 
 class Recette
 {
 	public :
 		Recette(int id);
-	
 		std::string nom_;
 		std::unique_ptr<Action> action_;	
+		
+		void afficher();
 };
-/*
+
 class Usine
 {
 	public :
@@ -109,11 +117,13 @@ class Usine
 	
 		std::unique_ptr<Departement> departement_;
 		int taille_;
-		std::unique_ptr<Departement> machines_;
-		std::vector<unique_ptr<Recette>> recettes_;
-		std::unique_ptr<QuantiteIngredient> stocks_;
+		std::vector<std::unique_ptr<Machine>> machines_;
+		std::vector<std::unique_ptr<Recette>> recettes_;
+		std::vector<std::unique_ptr<QuantiteIngredient>> stocks_;
+		
+		void afficher();
 };
-*/
+
 
 
 Ingredient::Ingredient(int id)
@@ -129,7 +139,10 @@ Prix::Prix(int id)
 	std::string lien = "http://localhost:8000/prix/" + std::to_string(id);
 	cpr::Response r  = cpr::Get(cpr::Url(lien));
 	json fich = json::parse(r.text);
+	
 	ingredient_ = std::make_unique<Ingredient>(fich["ingredient"]); 
+
+	
 	departement_ = std::make_unique<Departement>(fich["departement"]);
 	prix_ = fich["prix"];
 }
@@ -159,10 +172,45 @@ Action::Action(int id)
 	machine_ = std::make_unique<Machine>(fich["machine"]); 
 	commande_ = fich["commande"];
 	duree_ = fich["duree"];
-	ingredients_ = std::make_unique<QuantiteIngredient>(fich["ingredients"]); 
-	action_ = std::make_unique<Action>(fich["action"]); 
+
+	std::vector<std::unique_ptr<QuantiteIngredient>> ingredients_;
+
+	for (const auto &ingredient: (fich["ingredients"])) {
+		ingredients_.push_back(std::make_unique<QuantiteIngredient>(ingredient));
+	}
+	
+	action_ = std::make_unique<Action>(fich["action"]);
 }
 
+Recette::Recette(int id)
+{
+	std::string lien = "http://localhost:8000/recette/" + std::to_string(id);
+	cpr::Response r  = cpr::Get(cpr::Url(lien));
+	json fich = json::parse(r.text);
+	
+	nom_ = fich["nom"];
+	action_ = std::make_unique<Action>(fich["action"]);
+}
+
+Usine::Usine(int id)
+{
+	std::string lien = "http://localhost:8000/usine/" + std::to_string(id);
+	cpr::Response r  = cpr::Get(cpr::Url(lien));
+	json fich = json::parse(r.text);
+	
+	departement_ = std::make_unique<Departement>(fich["departement"]);
+	taille_ = fich["taille"];
+	
+	std::vector<std::unique_ptr<Machine>> machines_;
+	for (const auto &machine : (fich["machines"])) {
+		machines_.push_back(std::make_unique<Machine>(machine));
+	}
+	
+	std::vector<std::unique_ptr<QuantiteIngredient>> stocks_;
+	for (const auto &stock : (fich["stocks"])) {
+		stocks_.push_back(std::make_unique<QuantiteIngredient>(stock));
+	}
+}
 
 
 //Affichage
@@ -172,15 +220,64 @@ void Departement::afficher()
 	std::cout << "prixm2 : " << this->prixm2_ << std::endl;
 }
 
+void Ingredient::afficher()
+{
+	std::cout << "nom : " << this->nom_ << std::endl;
+}
 
+
+void Prix::afficher()
+{
+	std::cout << "ingredient : " << this->ingredient_->nom_ << std::endl;
+	std::cout << " departement : " << this->departement_->numero_ << std::endl;
+	std::cout << " prix : " << this->prix_ << std::endl;
+}
+
+
+void Machine::afficher()
+{
+	std::cout << "nom : " << this->nom_ << std::endl;
+	std::cout << "prix : " << this->prix_ << std::endl;
+}
+
+void QuantiteIngredient::afficher()
+{
+	std::cout << "ingredient : " << this->ingredient_->nom_ << std::endl;
+	std::cout << "quantite : " << this->quantite_ << std::endl;
+}
+
+
+
+void Action::afficher()
+{
+	std::cout << "machine : " << this->machine_->nom_ << std::endl;
+	std::cout << "commande : " << this->commande_ << std::endl;
+	std::cout << "duree : " << this->duree_ << std::endl;
+	std::cout << "ingredients : " << this->prixm2_ << std::endl;
+	std::cout << "action : " << this->prixm2_ << std::endl;
+}
+/*
+void Recette::afficher()
+{
+	std::cout << "numero : " << this->numero_ << std::endl;
+	std::cout << "prixm2 : " << this->prixm2_ << std::endl;
+}
+
+void Usine::afficher()
+{
+	std::cout << "numero : " << this->numero_ << std::endl;
+	std::cout << "prixm2 : " << this->prixm2_ << std::endl;
+}
+
+*/
 int main()
 {
 	/*
 	Departement dep(81, 2000);
 	dep.afficher();*/
-	
+	/*
 	// REQUETE HTTP
-	cpr::Response r  = cpr::Get(cpr::Url("http://localhost:8000/ingredient/1"));
+	cpr::Response r  = cpr::Get(cpr::Url("http://localhost:8000/action/1"));
 	//std::cout << r.status_code << "\n";
 	std::cout << "Reponse HTTP :" << std::endl;
     std::cout << r.text << "\n";     // JSON text string
@@ -189,15 +286,40 @@ int main()
     json ex1 = json::parse(r.text);
     std::cout << "Le JSON :" << std::endl;
     std::cout << ex1 << "\n";
+    */
     /*
     std::cout << "---------------------------------" << std::endl;
     std::cout << "Constructeur avec JSON :" << std::endl;
-    Departement dep2(ex1);
+    Action dep2(ex1);
     dep2.afficher();
     
     std::cout << "Constructeur avec ID :" << std::endl;
-	Departement dep3(1);
-    dep3.afficher();*/
+	Action dep3(1);
+    dep3.afficher();
+    */
+    
+    //Departement dep(1);
+    //dep.afficher();
+    std::cout << "---------------------------------" << std::endl;
+    Ingredient ing(1);
+    ing.afficher();
+    
+    std::cout << "---------------------------------" << std::endl;
+    Prix p(1);
+    p.afficher();
+    
+    std::cout << "---------------------------------" << std::endl;
+    Machine m(1);
+    m.afficher();
+    
+    std::cout << "---------------------------------" << std::endl;
+    QuantiteIngredient qi(3);
+    qi.afficher();
+    
+    std::cout << "---------------------------------" << std::endl;
+    Action a(1);
+    a.afficher();
+    
     
 	return 0;
 }
